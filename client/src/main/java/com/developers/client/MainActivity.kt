aea28f84877247579fbbcebfc8b1cecbc5882654
parayonid <yungfeTT15@gmail.com>
2026-04-14 12:56:37 -0600

    pan diferente 4

--- client/src/main/java/com/developers/client/MainActivity.kt
+++ client/src/main/java/com/developers/client/MainActivity.kt
@@ -25,7 +25,7 @@ class MainActivity : AppCompatActivity() {
         // 3. Datos únicos del CLIENTE
         val datosCliente = hashMapOf(
             "quien" to "CLIENTE APP ",
-            "accion" to "Quiero comprar panochon",
+            "accion" to "Quiero comprar panochita",
             "precio_visto" to 15.50,
             "fecha" to Date()
         )
