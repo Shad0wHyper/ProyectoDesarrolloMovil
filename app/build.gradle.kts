00436cffd2fe0e1cefbfc5985222298cd0ac8eb7
parayonid <yungfeTT15@gmail.com>
2026-09-17 08:18:44 -0600

    super commit AI BUNDLE

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,4 +60,6 @@ dependencies {
     implementation("com.google.android.gms:play-services-auth:21.0.0")
     implementation("androidx.core:core-splashscreen:1.0.1")
     implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 }
